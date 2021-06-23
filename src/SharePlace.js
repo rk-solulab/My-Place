3147b861a6f92c9ce624f3a09875aac66c4bd4de
Rishikesh Kale <rvk.rishikeshkale@gmail.com>
2021-06-23 11:36:19 +0530

    RK-33 Longitude and Latitude obtained

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,16 +1,34 @@
 class PlaceFinder {
-
     constructor() {
-        const getLocationForm = document.querySelector('form');
-        const submitFormButton = document.getElementById('locate-btn');
-        getLocationForm.addEventListner('click', this.getLocationHandler);
-        submitFormButton.addEventListener('submit', this.submitFormButtonHandler);
+      const getLocationForm = document.querySelector('form');
+      const locateUserButton = document.getElementById('locate-btn');
+      getLocationForm.addEventListener('submit', this.submitFormButtonHandler);
+      locateUserButton.addEventListener('click', this.locateUserHandler);
     }
-
-    getLocationHandler() {
-        
+  
+    locateUserHandler() {
+        console.log("Hello")
+      if (!navigator.geolocation) {
+        alert("Location feature not supported in your browser");
+        return;
+      }
+      navigator.geolocation.getCurrentPosition(
+        (successResult) => {
+            const coordinates = {
+                latitide : successResult.coords.latitude,
+                longitude: successResult.coords.longitude
+            };
+            console.log(coordinates);
+        },
+        (error) => {
+            alert('Could not locate your location please enter address manually')
+        }
+      );
     }
-    
-    submitFormButtonHandler() {}
-
-}
+  
+    submitFormButtonHandler() {
+        console.log("Hello")
+    }
+  }
+  //new PlaceFinder() : this is also sufficient to call the constructor of the function
+  const instantiatingPlaceFinderFunction = new PlaceFinder();
